98ed9bc9285bdf5058c80b4a65f265ad8292050a
trimscash <42578480+trimscash@users.noreply.github.com>
2022-01-07 22:36:05 +0900

    Update background.js

--- src/background.js
+++ src/background.js
@@ -8,11 +8,11 @@ var intervalID = window.setInterval(() => {
 }, 2000);
 
 function surprise() {
-	var imgNum=2;//0オリジン　　　いくつの画像ファイルを読み込むか
-	var soundNum=4;//0オリジンでっせ　　いくつの音声ファイルを読み込むか
-	var url = "./img/img" + getRandomInt(imgNum) + ".png";
+	var imgNum=3;//いくつの画像ファイルを読み込むか
+	var soundNum=5;//いくつの音声ファイルを読み込むか
+	var url = "./img/img" + getRandomInt(imgNum-1) + ".png";
 	// console.log(url);
-	var sound = new Audio("./sounds/sound" + getRandomInt(soundNum) + ".wav");
+	var sound = new Audio("./sounds/sound" + getRandomInt(soundNum-1) + ".wav");
 	var windowFeatures = "menubar=no,left=0,top=0,width=" + window.screen.width + ",height=" + window.screen.height + ",location=no,resizable=no,scrollbars=no,status=yes";
 	sound.play();
 	window.open(url, "imgswindow", windowFeatures);
